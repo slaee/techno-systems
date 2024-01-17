5f5a4e1957145fd2cea4921b2ae9713e56357291
albrecht0210 <albrecht.lanojan0210@gmail.com>
2024-01-18 07:34:39 +0800

    Add Teknoplat Model Serializers to __init__.py

--- backend/backend/api/serializers/__init__.py
+++ backend/backend/api/serializers/__init__.py
@@ -24,3 +24,15 @@ from .ClassRoomPESerializer import ClassRoomPETakerSerializer
 
 from .PeerEvalSerializer import PeerEvalSerializer
 from .PeerEvalSerializer import AssignPeerEvalSerializer
+
+from .ChatbotSerializer import ChatbotSerializer
+from .CriteriaSerializer import CriteriaSerializer
+from .FeedbackSerializer import FeedbackSerializer
+from .MeetingSerializer import MeetingSerializer
+from .MeetingCommentSerializer import MeetingCommentSerializer
+from .MeetingCriteriaSerializer import MeetingCriteriaSerializer
+from .MeetingPresentorSerializer import MeetingPresentorSerializer
+from .MessageSerializer import MessageSerializer
+from .PitchSerializer import PitchSerializer
+from .RatingSerializer import RatingSerializer
+from .RemarkSerializer import RemarkSerializer
