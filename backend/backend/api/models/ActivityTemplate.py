3005659089f17a7e3b7219a9212a06e9b440dcc4
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2024-01-17 18:47:40 +0800

    Change ActivityTemplate

--- backend/backend/api/models/ActivityTemplate.py
+++ backend/backend/api/models/ActivityTemplate.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Template(models.Model):
+class ActivityTemplate(models.Model):
     course_name = models.TextField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=10000)
