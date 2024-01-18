9ec245dd9e4e51f0fbcac1e31b581df147ae6b61
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2024-01-18 17:36:51 +0800

    Modified serializers

--- backend/backend/api/serializers/ActivityCommentSerializer.py
+++ backend/backend/api/serializers/ActivityCommentSerializer.py
@@ -17,12 +17,35 @@ class CommentCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ActivityComment
-        fields = ('id', 'activity_id', 'user_id', 'comment', 'date_created')  # Include the user field
+        fields = ('id', 'activity_id', 'user_id', 'comment', 'date_created')  
+    
+    def create(self, validated_data):
+        user_id = validated_data.pop('user_id', None)
+        activity_id = validated_data.pop('activity_id', None)
+
+        if not user_id:
+            raise serializers.ValidationError({'error': 'User ID not provided'})
+        if not activity_id:
+            raise serializers.ValidationError({'error': 'Activity ID not provided'})
+
+        try:
+            user = User.objects.get(pk=user_id)
+            activity = Activity.objects.get(pk=activity_id)
+
+            # Create the ActivityComment instance using the actual instances
+            comment = ActivityComment.objects.create(user_id=user, activity_id=activity, **validated_data)
+
+            return comment
+        except User.DoesNotExist:
+            raise serializers.ValidationError({'error': 'User not found'})
+        except Activity.DoesNotExist:
+            raise serializers.ValidationError({'error': 'Activity not found'})
+
 
 class ActivityCommentSerializer(serializers.ModelSerializer):
-    activity = SpecificActivityCommentSerializer()
-    user = UserCommentSerializer()
+    # activity = SpecificActivityCommentSerializer()
+    # user = UserCommentSerializer()
 
     class Meta:
         model = ActivityComment
-        fields = ('id', 'comment', 'date_added', 'activity', 'user')
+        fields = ('id', 'comment', 'date_created', 'activity_id', 'user_id')
--- backend/backend/api/serializers/ActivityWorkAttachmentSerializer.py
+++ backend/backend/api/serializers/ActivityWorkAttachmentSerializer.py
@@ -3,12 +3,9 @@ from api.models import ActivityWorkAttachment
 from api.models import Activity
 
 class ActivityWorkAttachmentSerializer(serializers.ModelSerializer):
-    activity_id = serializers.IntegerField(write_only=True)
-
     class Meta:
         model = ActivityWorkAttachment
         fields = ['id', 'activity_id', 'description', 'file_attachment', 'date_created']
-        read_only_fields = ['date_created'] 
 
     def create(self, validated_data):
         activity_id = validated_data.pop('activity_id')
@@ -19,7 +16,6 @@ class ActivityWorkAttachmentSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Activity not found")
 
         # Create the Work instance with the associated Activity
-        work = ActivityWorkAttachment(**validated_data)
-        work.activity = activity
+        work = ActivityWorkAttachment(activity_id=activity, **validated_data)
         work.save()
         return work
