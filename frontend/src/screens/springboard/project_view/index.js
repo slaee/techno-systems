858c4f263acb42f73981f48d44bfd93aa5d65a98
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-01-21 02:58:09 +0800

    clean project view

--- frontend/src/screens/springboard/project_view/index.js
+++ frontend/src/screens/springboard/project_view/index.js
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useOutletContext } from 'react-router-dom';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import ProjectContents from '../components/ProjectDetails/ProjectContent';
-import { useClassMemberTeam, useProjects, useBoardTemplate } from '../../../hooks';
 
 function ProjectView(props) {
-  const { user, classId, classRoom, classMember } = useOutletContext();
-  const { team } = useClassMemberTeam(classId, classMember?.id);
-
   const { projId } = useParams();
 
   return (
