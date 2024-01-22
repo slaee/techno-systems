1188d7ff207ceb1ffcde1a67035f51e5e5b5bd1f
albrecht0210 <albrecht.lanojan0210@gmail.com>
2024-01-23 05:46:50 +0800

    add full_name field

--- backend/backend/api/serializers/MeetingCommentSerializer.py
+++ backend/backend/api/serializers/MeetingCommentSerializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 
 from api.models import MeetingComment
+from api.models import User
+from api.models import ClassMember
 
 class MeetingCommentSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = MeetingComment
         fields = '__all__'
+
+    def get_full_name(self, obj):
+        classmember = ClassMember.objects.get(id=obj.classmember_id)
+        return User.objects.get(id=classmember.user_id).get_full_name()
+        
