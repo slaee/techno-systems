aee570abb7960a92c1e75e5803dbc4d00f9ac7a6
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-01-22 09:46:58 +0800

    add search project

---
+++ frontend/src/screens/springboard/search_project/SearchProject.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useParams, useNavigate, useOutletContext } from 'react-router-dom';
+import { IoArrowBackSharp } from 'react-icons/io5';
+import ProjectContents from '../components/ProjectDetails/ProjectContent';
+
+function SearchProject() {
+  const { user, classId, classRoom } = useOutletContext();
+  const { projId } = useParams();
+  const navigate = useNavigate();
+  const isClass = user.role === 1;
+
+  const goBack = () => {
+    navigate(`/classes/${classId}/allprojects`);
+  };
+
+  return (
+    <div className="px-5 d-flex justify-content-start">
+      <span style={{ cursor: 'pointer' }} onClick={goBack}>
+        <IoArrowBackSharp />
+      </span>
+      <ProjectContents selected={projId} isClass={isClass} />
+    </div>
+  );
+}
+
+export default SearchProject;
