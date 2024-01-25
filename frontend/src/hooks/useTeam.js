417a62ed2ed3888a984a352e0201ede2d9b9cd54
albrecht0210 <albrecht.lanojan0210@gmail.com>
2024-01-25 14:13:22 +0800

    add a teamMembers state

--- frontend/src/hooks/useTeam.js
+++ frontend/src/hooks/useTeam.js
@@ -6,6 +6,7 @@ const useTeam = (classId, teamId) => {
   const navigate = useNavigate();
   const [isRetrieving, setIsRetrieving] = useState(true);
   const [team, setTeam] = useState(null);
+  const [teamMembers, setTeamMembers] = useState(null);
 
   useEffect(() => {
     const get = async () => {
@@ -33,14 +34,39 @@ const useTeam = (classId, teamId) => {
           break;
         default:
       }
+    };
 
-      setIsRetrieving(false);
+    const teamMembers = async () => {
+      let responseCode;
+      let retrievedMembers;
+  
+      try {
+        const res = await ClassRoomsService.teamMembers(classId, teamId);
+        responseCode = res?.status;
+        retrievedMembers = res?.data;
+      } catch (error) {
+        responseCode = error?.response?.status;
+      }
+  
+      switch (responseCode) {
+        case 200:
+          setTeamMembers(retrievedMembers);
+        case 404:
+          navigate(`/classes/${classId}/teams`);
+          break;
+        case 500:
+          navigate('/classes');
+          break;
+        default:
+      }
     };
 
     get();
+    teamMembers();
+    setIsRetrieving(false);
   }, []);
 
-  return { isRetrieving, team };
+  return { isRetrieving, team, teamMembers };
 };
 
 export default useTeam;
