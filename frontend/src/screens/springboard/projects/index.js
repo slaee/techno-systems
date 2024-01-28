ba63af2417b11e4a20e55be7e560be08e8d67edc
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-01-28 22:36:30 +0800

    handle errors for create proj and green bullet for active proj

--- frontend/src/screens/springboard/projects/index.js
+++ frontend/src/screens/springboard/projects/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useOutletContext, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTrash, faCircle } from '@fortawesome/free-solid-svg-icons';
 import Swal from 'sweetalert2';
+import Loading from '../components/UI/Loading/Loading';
 import Card from '../components/UI/Card/Card';
 import { useClassMemberTeam, useProjects } from '../../../hooks';
 import 'primeicons/primeicons.css';
@@ -32,16 +33,8 @@ function SpringBoardProjects() {
   }, [team?.id, refresh]);
 
   const showCreateProjectModal = () => {
-    if (projects.length >= 3) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Project Limit Reached',
-        html: 'You have reached the project limit.<br>Only 3 projects per group are allowed.',
-        confirmButtonColor: '#8A252C',
-      });
-    } else {
-      Swal.fire({
-        html: `
+    Swal.fire({
+      html: `
         <span style="font-size: 20px">Create a New Project</span>
         <br>
         <input type="text" id="input1" placeholder="Enter new project name" class="swal2-input" style="height: 35px; width: 86%; font-size: 16px; font-family: 'Calibri', sans-serif; display: flex;"/>
@@ -49,21 +42,28 @@ function SpringBoardProjects() {
         <textarea id="input2" placeholder="Enter project description" class="swal2-textarea" style="margin: 0 auto; width: 86%; height: 100px; resize: none; font-size: 16px; font-family: 'Calibri', sans-serif;" maxlength="200"></textarea>
         <div id="charCount" style="text-align: right; color: #555; font-size: 12px; margin-top: 5px;">0/200 characters</div>
         `,
-        showCancelButton: true,
-        confirmButtonText: 'Create',
-        confirmButtonColor: '#9c7b16',
-        cancelButtonText: 'Cancel',
-        cancelButtonColor: 'rgb(181, 178, 178)',
-        preConfirm: async () => {
-          const input1Value = document.getElementById('input1').value;
-          const input2Value = document.getElementById('input2').value;
-          try {
-            if (!input1Value) {
-              throw new Error('Project name cannot be empty');
-            } else if (!input2Value) {
-              throw new Error('Please enter the project description.');
-            }
-            await createProjects({
+      showCancelButton: true,
+      confirmButtonText: 'Create',
+      confirmButtonColor: '#9c7b16',
+      cancelButtonText: 'Cancel',
+      cancelButtonColor: 'rgb(181, 178, 178)',
+      preConfirm: async () => {
+        const input1Value = document.getElementById('input1').value;
+        const input2Value = document.getElementById('input2').value;
+        const wordsArray = input2Value.split(/\s+/);
+        try {
+          if (!input1Value && !input2Value) {
+            Swal.showValidationMessage('Project name and description cannot be empty');
+          } else if (!input1Value) {
+            Swal.showValidationMessage('Project name cannot be empty');
+          } else if (!input2Value) {
+            Swal.showValidationMessage('Please enter the project description.');
+          } else if (wordsArray.length < 10 || wordsArray.length > 50) {
+            Swal.showValidationMessage(
+              `Description should have 10 - 50 words. You have ${wordsArray.length} word/s.`
+            );
+          } else {
+            const response = await createProjects({
               body: {
                 name: input1Value,
                 description: input2Value,
@@ -72,25 +72,25 @@ function SpringBoardProjects() {
                 is_active: false,
               },
             });
-            return true;
-          } catch (error) {
-            Swal.showValidationMessage(
-              `Project with the name '${input1Value}' already exists. Please enter another project name.`
-            );
-            return false;
+
+            if (response) {
+              Swal.fire({
+                title: 'Project Created',
+                icon: 'success',
+                confirmButtonColor: '#9c7b16',
+              });
+              setRefresh(!refresh);
+            } else {
+              Swal.showValidationMessage(
+                `Project with the name '${input1Value}' already exists. Please enter another project name.`
+              );
+            }
           }
-        },
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: 'Project Created',
-            icon: 'success',
-            confirmButtonColor: '#9c7b16',
-          });
-          setRefresh(!refresh);
+        } catch (error) {
+          Swal.showValidationMessage(`Error: Creating project error;`);
         }
-      });
-    }
+      },
+    });
 
     const input2 = document.getElementById('input2');
     const charCount = document.getElementById('charCount');
@@ -155,6 +155,10 @@ function SpringBoardProjects() {
     navigate(`/classes/${classId}/project/${projId}`);
   };
 
+  if (!projects) {
+    return <Loading />;
+  }
+
   if (!team) {
     return (
       <div className="px-5">
@@ -167,10 +171,6 @@ function SpringBoardProjects() {
         </div>
       </div>
     );
-  }
-
-  if (!projects) {
-    return <p>Loading</p>;
   }
 
   return (
@@ -192,7 +192,12 @@ function SpringBoardProjects() {
             <div key={project.id}>
               <Card className={styles.cardProj} onClick={() => onNavigate(project.id)}>
                 <div className={styles.topCon}>
-                  <h2>{project.name}</h2>
+                  <div className="d-flex align-items-center">
+                    {project.is_active && (
+                      <FontAwesomeIcon icon={faCircle} className={styles.greenBullet} size="xs" />
+                    )}
+                    <h2>{project.name}</h2>
+                  </div>
                   <span
                     onClick={(event) => {
                       event.stopPropagation();
