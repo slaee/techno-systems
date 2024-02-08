9ea36fca7f63d50e15e4ed157a0740eecd1cbf67
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2024-02-08 09:59:57 +0800

    Update with &&

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,9 +2,7 @@
 pip3 install -r /requirements.txt
 
 # May as well do this too, while we're here.
-python manage.py makemigrations && python manage.py migrate
-
-python manage.py loaddata criteria_fixture.json
+python manage.py makemigrations && python manage.py migrate && python manage.py loaddata criteria_fixture.json
 
 # execs $CMD
 exec "$@"
